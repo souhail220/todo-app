94576e32b7677d6b054253c9145305ada74ee891
souhail220 <souhailgana943@gmail.com>
2024-11-22 21:52:04 +0100

    created mongoose connection and crud paths

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = 8000;
 
 // init app
 const app = express();
 
+const connectionUrl = "mongodb://localhost:27017/todoDb";
+
+mongoose
+  .connect(connectionUrl)
+  .then(() => console.log("DataBase connection successful"))
+  .catch((error) => console.log(error.message));
+
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  try {
+    res.render("index");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/add-todo", (req, res) => {
+  try {
+    res.render("newTodo");
+  } catch (error){
+    res.status(500).json({message: error.message});
+  }
+})
+
+app.get("/update-todo", (req, res) => {
+  try{
+    res.render("updateTodo");
+  } catch (error){
+    res.status(500).json({message: error.message});
+  }
+})
+
+app.get("/delete-todo", (req, res) => {
+  try{
+    res.render("deleteTodo");
+  } catch (error){
+    res.status(500).json({message: error.message});
+  }
+})
 
 // listen to server
 app.listen(PORT, () => {
